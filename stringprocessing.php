436147f0214f3c2265803bbb7cd1d6b3f3d8038a
Joeson Stanes <joesonstanes1360@gmail.com>
2024-10-01 04:06:07 +0530

    g

---
+++ stringprocessing.php
@@ -0,0 +1,10 @@
+<?php
+$string="redefine";
+$pattern="/def/";
+if(preg_match($pattern,$string)){
+    echo "pattern $pattern found in $string /n";
+
+}
+else{
+    echo "pattern $pattern not found in $string \n"
+}
